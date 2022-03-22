5a5dd2472c3401994ab77616c629beb3dbfc5ae7
Hongyuan Mei <hongyuanmei@gmail.com>
2022-03-22 16:04:53 -0500

    add adaptive input, adaptive softmax, gradient checkpointing

---
+++ transformerlm/adasoftmax.py
@@ -0,0 +1,308 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import functools
+import operator
+
+# learn from: 
+# fairseq: https://github.com/pytorch/fairseq/blob/d421749323/fairseq/modules/adaptive_softmax.py
+# pytorch: https://pytorch.org/docs/stable/_modules/torch/nn/modules/adaptive.html#AdaptiveLogSoftmaxWithLoss
+# use fairseq init to facilitate parameter sharing with adaptive input
+# use pytorch forward since it is more efficient (50% memory reduction) and works well with DDP
+
+class TiedLinear(nn.Module):
+    def __init__(self, weight, transpose):
+        super().__init__()
+        self.weight = weight
+        self.transpose = transpose
+
+    def forward(self, input):
+        return F.linear(input, self.weight.t() if self.transpose else self.weight)
+
+
+class TiedHeadModule(nn.Module):
+    def __init__(self, weights, input_dim, num_classes):
+        super().__init__()
+        tied_emb, _ = weights
+        self.num_words, emb_dim = tied_emb.size()
+
+        self.word_proj = TiedLinear(tied_emb, transpose=False)
+        if input_dim != emb_dim:
+            self.word_proj = nn.Sequential(
+                nn.Linear(input_dim, emb_dim, bias=False),
+                self.word_proj,
+            )
+
+        self.class_proj = nn.Linear(input_dim, num_classes, bias=False)
+        self.out_dim = self.num_words + num_classes
+
+        self.register_buffer("_float_tensor", torch.FloatTensor(1))
+
+    def forward(self, input):
+        inp_sz = functools.reduce(operator.mul, input.shape[:-1], 1)
+        out = self._float_tensor.new(inp_sz, self.out_dim)
+        out[:, : self.num_words] = self.word_proj(input.view(inp_sz, -1))
+        out[:, self.num_words :] = self.class_proj(input.view(inp_sz, -1))
+        return out
+
+
+class AdaptiveSoftmax(nn.Module):
+    """
+    This is an implementation of the efficient softmax approximation for
+    graphical processing units (GPU), described in the paper "Efficient softmax
+    approximation for GPUs" (http://arxiv.org/abs/1609.04309).
+    """
+
+    def __init__(
+        self,
+        vocab_size,
+        input_dim,
+        cutoff,
+        dropout,
+        factor=4.0,
+        adaptive_inputs=None,
+        tie_proj=False,
+    ):
+        super().__init__()
+
+        if vocab_size > cutoff[-1]:
+            cutoff = cutoff + [vocab_size]
+        else:
+            assert (
+                vocab_size == cutoff[-1]
+            ), "cannot specify cutoff larger than vocab size"
+
+        output_dim = cutoff[0] + len(cutoff) - 1
+
+        self.vocab_size = vocab_size
+        self.cutoff = cutoff
+        self.dropout_module = nn.Dropout(dropout)
+        self.input_dim = input_dim
+        self.factor = factor
+
+        self.lsm = nn.LogSoftmax(dim=1)
+
+        if adaptive_inputs is not None:
+            self.head = TiedHeadModule(
+                adaptive_inputs.weights_for_band(0),
+                input_dim,
+                len(cutoff) - 1,
+            )
+        else:
+            self.head = nn.Linear(input_dim, output_dim, bias=False)
+        
+        self._make_tail(adaptive_inputs, tie_proj)
+
+        def init_weights(m):
+            if (
+                hasattr(m, "weight")
+                and not isinstance(m, TiedLinear)
+                and not isinstance(m, TiedHeadModule)
+            ):
+                nn.init.xavier_uniform_(m.weight)
+
+        self.apply(init_weights)
+
+        # above is all from fairseq 
+        # below is from pytorch (except register_buffer)
+
+        self.cutoffs = self.cutoff #+ [self.vocab_size]
+        self.shortlist_size = self.cutoff[0]
+
+        self.register_buffer("version", torch.LongTensor([1]))
+
+    def _make_tail(self, adaptive_inputs=None, tie_proj=False):
+        self.tail = nn.ModuleList()
+        for i in range(len(self.cutoff) - 1):
+            dim = int(self.input_dim // self.factor ** (i + 1))
+
+            tied_emb, tied_proj = (
+                adaptive_inputs.weights_for_band(i + 1)
+                if adaptive_inputs is not None
+                else (None, None)
+            )
+
+            if tied_proj is not None:
+                if tie_proj:
+                    proj = TiedLinear(tied_proj, transpose=True)
+                else:
+                    proj = nn.Linear(tied_proj.size(0), tied_proj.size(1), bias=False)
+            else:
+                proj = nn.Linear(self.input_dim, dim, bias=False)
+
+            if tied_emb is None:
+                out_proj = nn.Linear(
+                    dim, self.cutoff[i + 1] - self.cutoff[i], bias=False
+                )
+            else:
+                out_proj = TiedLinear(tied_emb, transpose=False)
+
+            m = nn.Sequential(
+                proj,
+                nn.Dropout(self.dropout_module.p),
+                out_proj,
+            )
+
+            self.tail.append(m)
+
+    def upgrade_state_dict_named(self, state_dict, name):
+        version_name = name + ".version"
+        if version_name not in state_dict:
+            raise Exception("This version of the model is no longer supported")
+
+    
+    def forward(self, input, target): 
+        
+        if input.size(0) != target.size(0):
+            raise RuntimeError('Input and target should have the same size '
+                               'in the batch dimension.')
+
+        used_rows = 0
+        batch_size = target.size(0)
+
+        output = input.new_zeros(batch_size)
+        gather_inds = target.new_empty(batch_size)
+
+        cutoff_values = [0] + self.cutoffs
+
+        # hack : to use all params 
+        # to use pytorch DDP, we have to use all params of model 
+        # to not affect the actual result, we choose to access all params 
+        # and then accumulate them and multiply by 0.0
+        tmp = 0.0 
+        tmpind = torch.zeros((self.input_dim,), dtype=torch.float32).cuda()
+
+        for i in range(len(cutoff_values) - 1):
+
+            if i == 0: 
+                # access head 
+                tmp += self.head(tmpind).sum()
+            else: 
+                # access tail 
+                tmp += self.tail[i-1](tmpind).sum()
+
+            low_idx = cutoff_values[i]
+            high_idx = cutoff_values[i + 1]
+
+            target_mask = (target >= low_idx) & (target < high_idx)
+            row_indices = target_mask.nonzero().squeeze()
+
+            if row_indices.numel() == 0:
+                continue
+
+            if i == 0:
+                gather_inds.index_copy_(0, row_indices, target[target_mask])
+
+            else:
+                relative_target = target[target_mask] - low_idx
+                input_subset = input.index_select(0, row_indices)
+
+                cluster_output = self.tail[i - 1](input_subset)
+                cluster_index = self.shortlist_size + i - 1
+
+                gather_inds.index_fill_(0, row_indices, cluster_index)
+
+                cluster_logprob = F.log_softmax(cluster_output, dim=1)
+                local_logprob = cluster_logprob.gather(1, relative_target.unsqueeze(1))
+                output.index_copy_(0, row_indices, local_logprob.squeeze(1))
+
+            used_rows += row_indices.numel()
+
+        if used_rows != batch_size:
+            raise RuntimeError("Target values should be in [0, {}], "
+                               "but values in range [{}, {}] "
+                               "were found. ".format(self.vocab_size - 1,
+                                                     target.min().item(),
+                                                     target.max().item()))
+
+        head_output = self.head(input)
+        head_logprob = F.log_softmax(head_output, dim=1)
+        output += head_logprob.gather(1, gather_inds.unsqueeze(1)).squeeze()
+        loss = (-output).mean()
+        
+        loss += tmp * 0.0 
+
+        return output, loss
+
+
+def main(): 
+
+    import gc
+    from .adainput import AdaptiveInput
+    import random
+
+    gc.collect()
+    torch.cuda.empty_cache()
+    torch.cuda.reset_max_memory_allocated()
+
+    print(f"test adaptive softmax")
+
+    vocab_size = 267744
+    padding_idx = 0
+    dim = 1024
+    T = 3072
+    factor = 4
+    cutoff = [20000, 60000]
+
+    torch.manual_seed(0)
+    random.seed(0)
+
+    adainput = AdaptiveInput(
+        vocab_size = vocab_size,
+        padding_idx = padding_idx,
+        initial_dim = dim,
+        factor = factor,
+        output_dim = dim,
+        cutoff = cutoff,
+    ).cuda()
+
+    torch.manual_seed(0)
+    random.seed(0)
+
+    adasoftmax = AdaptiveSoftmax(
+        vocab_size = vocab_size,
+        input_dim = dim,
+        cutoff = cutoff,
+        dropout = 0.1,
+        factor = factor,
+        adaptive_inputs = adainput,
+        tie_proj=True,
+    ).cuda()
+
+    print("\nadaptive softmax")
+    for k, v in adasoftmax.state_dict().items(): 
+        print(f"{k}  :  {v.size()}")
+
+    torch.manual_seed(0)
+    random.seed(0)
+
+    x = torch.randint(0, 267744, (1, T,), dtype=torch.int64).cuda()
+    h = adainput(x)
+    print(f"\nh")
+    print(f"size is : {h.size()}")
+
+    h = h.contiguous().view(-1, dim)
+    x = x.contiguous().view(-1)
+
+    outs, loss = adasoftmax(h, x)
+    
+    print("\nlogprob")
+    print(f"logprob size : {outs.size()}")
+
+    print("\nshow some logprobs")
+    print(outs[:10])
+
+    print("\nloss")
+    print(loss)
+
+    # track memory
+    torch.cuda.synchronize()
+    print("\nMax memory used by tensors")
+    xxx = torch.cuda.max_memory_allocated()
+    ggg = xxx//1000000000
+    mmm = (xxx-ggg*1000000000) // 1000000
+    print(f"{xxx}B")
+    print(f"{ggg}GB-{mmm} MB")
+
+if __name__ == "__main__": main()
--- transformerlm/model.py
+++ transformerlm/model.py
@@ -1,68 +1,74 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils
+import torch.utils.checkpoint
 import math
 
 from utils import clones, attention
+from adainput import AdaptiveInput
+from adasoftmax import AdaptiveSoftmax
 
 # in-house implementation of Transformer
 # largely reply on Susha Rush annotated transformer: https://nlp.seas.harvard.edu/2018/04/03/attention.html
 # but this model is a generative model 
 
+# also consult fairseq source code
+# since that code achieves SOTA on wikitext and enwik
+# https://github.com/pytorch/fairseq/blob/d421749323/fairseq/modules/transformer_layer.py
+# eps. adaptive input & softmax, order of operation, ...
+# also uses gradient checkpointing
+
 class TransformerLM(nn.Module): 
 
-    def __init__(self, vocab_size, 
-        n_layers, d_emb, d_model, n_heads, d_ffn, d_softmax, tie_emb, dropout):
+    def __init__(self, 
+        vocab_size, n_layers, d_model, n_heads, d_ffn, 
+        ada, adacutoff, pad_idx, tie_emb, dropout):
         super(TransformerLM, self).__init__()
         # hyper-params
         self.vocab_size = vocab_size
         self.n_layers = n_layers
-        self.d_emb = d_emb
         self.d_model = d_model 
         self.n_heads = n_heads
         self.d_ffn = d_ffn
-        self.d_softmax = d_softmax
+        self.scaling = math.sqrt(d_model)
+        self.ada = ada 
+        self.adacutoff = sorted(adacutoff)
+        self.pad_idx = pad_idx
         self.tie_emb = tie_emb
         self.dropout = dropout
 
-        self.embed = Embeddings(vocab_size, d_emb)
-        # d_ff = d_model is our simplification
-        if d_emb != d_model: # word emb dim not match model dim 
-            self.input_proj = nn.Linear(d_emb, d_model, bias=False)
-        self.position = PositionEmbedding(d_model, dropout)
-        self.decoder = Decoder(n_layers, d_model, n_heads, d_ffn, dropout)
-        if d_model != d_softmax: # output layer emb dim not match model dim
-            self.output_proj = nn.Linear(d_model, d_softmax, bias=False)
-        self.generator = Generator(d_softmax, vocab_size)
-
-        if self.tie_emb == 1: 
-            # tie input and out embeddings
-            # if we use adaptive softmax, we also have to use adaptive input
-            assert d_emb == d_softmax, f"dimension mismatch: d_emb={d_emb} d_softmax={d_softmax}"
-            self.generator.tie(self.embed.get_weight())
-        elif self.tie_emb == 0: 
-            pass # do nothing 
+        # input embeddings : adaptive or not 
+        if ada: 
+            self.embed = AdaptiveInput(
+                vocab_size, pad_idx, d_model, 4.0, d_model, self.adacutoff)
         else: 
-            raise Exception(f"Unknown tie_emb : {self.tie_emb}")
-
+            self.embed = Embeddings(vocab_size, d_model)
+        # positional encoding
+        self.position = PositionEmbedding(d_model, dropout)
+        # decoder : Transformer layers
+        self.decoder = Decoder(n_layers, d_model, n_heads, d_ffn, dropout)
+        # softmax : adaptive or not 
+        if ada: 
+            tie_proj = True if tie_emb else False 
+            self.generator = AdaptiveSoftmax(
+                vocab_size, d_model, self.adacutoff, dropout, 4.0, self.embed, tie_proj)
+        else: 
+            self.generator = Generator(d_model, vocab_size)
+            if tie_emb: 
+                self.generator.tie(self.embed.get_weight())
+        
         # according to Sasha, this is important 
         for p in self.parameters(): 
             if p.dim() > 1: 
                 nn.init.xavier_uniform(p)
 
-    def forward(self, sent, mask, output='prob'):
+    def forward(self, sent, mask):
         x = self.embed(sent)
-        if self.d_emb != self.d_model: 
-            x = self.input_proj(x)
-        hidden_state = self.decoder(self.position(x), mask)
-        if output == 'hidden': 
-            return hidden_state
-        elif output == 'prob': 
-            if self.d_model != self.d_softmax: 
-                hidden_state = self.output_proj(hidden_state)
-            return self.generator(hidden_state)
-        else: 
-            raise Exception(f"Unknown output type : {output}")
+        x *= self.scaling # fairseq hack
+        x = self.position(x)
+        hidden_state = self.decoder(x, mask)
+        return hidden_state
 
 class Generator(nn.Module): 
 
@@ -74,24 +80,7 @@ class Generator(nn.Module):
         return F.log_softmax(self.linear(x), dim=-1)
     
     def tie(self, weight): 
-        #print(weight.shape)
-        #print(self.linear.weight.shape)
         self.linear.weight = weight
-
-
-class SublayerConnection(nn.Module): 
-    """
-    A residual connection followed by a layer norm.
-    Note for code simplicity the norm is first as opposed to last.
-    """
-    def __init__(self, d_model, dropout):
-        super(SublayerConnection, self).__init__()
-        self.norm = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x, sublayer):
-        "Apply residual connection to any sublayer with the same size."
-        return x + self.dropout(sublayer(self.norm(x)))
 
 
 class Decoder(nn.Module): 
@@ -100,26 +89,49 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.layers = nn.ModuleList(
             [ DecoderLayer(d_model, n_heads, d_ffn, dropout) for _ in range(n_layers) ])
-        self.norm = nn.LayerNorm(d_model)
-    
+
     def forward(self, x, mask): 
         for layer in self.layers: 
-            x = layer(x, mask)
-        return self.norm(x)
+            # gradient checkpointing
+            def create_custom_forward(module): 
+                def custom_forward(y): 
+                    return module(y, mask)
+                return custom_forward
+            # gradient checkpoing trick to save GPU memory
+            # https://medium.com/tensorflow/fitting-larger-networks-into-memory-583e3c758ff9
+
+            x = torch.utils.checkpoint.checkpoint(
+                create_custom_forward(layer), x)
+            #x = layer(x, mask)
+        return x 
 
 
 class DecoderLayer(nn.Module): 
+    # we consult fairseq for tech details
+    # https://github.com/pytorch/fairseq/blob/d421749323/fairseq/modules/transformer_layer.py
 
     def __init__(self, d_model, n_heads, d_ffn, dropout): 
         super(DecoderLayer, self).__init__()
         self.attn = MultiHeadedAttention(d_model, n_heads, dropout)
         self.ffn = PositionWiseFeedForward(d_model, d_ffn, dropout)
-        self.sublayers = nn.ModuleList(
-            [ SublayerConnection(d_model, dropout) for _ in range(2) ])
-    
+        self.attn_norm = nn.LayerNorm(d_model)
+        self.ffn_norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x, mask): 
-        x = self.sublayers[0](x, lambda x: self.attn(x, x, x, mask))
-        return self.sublayers[1](x, self.ffn)
+        # attn block 
+        residual = x 
+        x = self.attn_norm(x)
+        x = self.attn(x, x, x, mask)
+        x = self.dropout(x)
+        x = x + residual
+        # ffn block 
+        residual = x 
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = self.dropout(x)
+        x = x + residual
+        return x 
 
 
 class MultiHeadedAttention(nn.Module): 
@@ -127,34 +139,51 @@ class MultiHeadedAttention(nn.Module):
     def __init__(self, d_model, n_heads, dropout): 
         super(MultiHeadedAttention, self).__init__()
         assert d_model % n_heads == 0, "dim(model) must be a multiple of num(head)"
+        self.d_model = d_model
         self.d_key = d_model // n_heads
         self.n_heads = n_heads
-        self.linears = nn.ModuleList(
-            [ nn.Linear(d_model, d_model) for _ in range(4) ])
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.out_proj = nn.Linear(d_model, d_model)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, query, key, value, mask=None):
-        if mask is not None: 
-            # same mask applied to all h heads 
-            mask = mask.unsqueeze(1)
-        batch_size = query.size(0)
-
-        # do linear proj in batch from d_model -> h x d_key 
-        query, key, value = [
-            l(x).view(batch_size, -1, self.n_heads, self.d_key).transpose(1,2) \
-            for l, x in zip(self.linears, (query, key, value))
-        ]
-
-        # apply attention on all the proj vectors in batch 
-        x, self.attn = attention(
-            query, key, value, mask=mask, dropout=self.dropout )
-        
-        # concat and linear proj
-        x = x.transpose(1, 2).contiguous().view(
-            batch_size, -1, self.n_heads * self.d_key )
-
-        return self.linears[-1](x)
-
+    def forward(self, query, key, value, mask=None): 
+        """
+        adapted from fairseq 
+        https://github.com/pytorch/fairseq/blob/main/fairseq/modules/multihead_attention.py#L308-L330
+        """
+        assert mask is not None 
+        query = query.transpose(0, 1)
+        key = key.transpose(0, 1)
+        value = value.transpose(0, 1)
+        attn_mask = ~mask[0,:,:]
+        # multi_head_atten_forward uses seq len as dim-0 and batch size as dim-1
+        attn_out, _ = F.multi_head_attention_forward(
+            query, 
+            key, 
+            value, 
+            self.d_model, 
+            self.n_heads, 
+            torch.empty([0]).cuda(), 
+            torch.cat((self.q_proj.bias, self.k_proj.bias, self.v_proj.bias)), 
+            None, # self.bias_k, 
+            None, # self.bias_v, 
+            False, # self.add_zero_attn
+            self.dropout.p, # should be 0.1
+            self.out_proj.weight, 
+            self.out_proj.bias, 
+            self.training, # self.training or self.dropout_module.apply_during_inference 
+            None, # key_padding_mask 
+            False, # need_weights 
+            attn_mask, # attn_mask
+            use_separate_proj_weight=True,
+            q_proj_weight=self.q_proj.weight,
+            k_proj_weight=self.k_proj.weight,
+            v_proj_weight=self.v_proj.weight,
+        )
+        attn_out = attn_out.transpose(0, 1)
+        return attn_out
     
 class PositionWiseFeedForward(nn.Module): 
     
@@ -177,31 +206,73 @@ class Embeddings(nn.Module):
     
     def forward(self, x): 
         # NOTE: not sure if we really need to scale by sqrt(d_model)
-        return self.lut(x) # * math.sqrt(self.d_model)
+        return self.lut(x) * math.sqrt(self.d_model)
     
     def get_weight(self): 
         return self.lut.weight
 
 
 class PositionEmbedding(nn.Module): 
-
+    """
+    fairseq position embedding
+    https://github.com/pytorch/fairseq/blob/b554f5ec9067c2f88bf17a736ddb57ea1cab14cc/fairseq/modules/sinusoidal_positional_embedding.py#L15
+    """
     def __init__(self, d_model, dropout, max_len=5000): 
         super(PositionEmbedding, self).__init__()
+        # dropout is never used
+        # we keep it in arg to match interface
         self.dropout = nn.Dropout(dropout)
-
-        # compute the positional embeddings once in log space 
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * \
-            -(math.log(10000.0) / d_model)
+        self.embedding_dim = d_model
+        self.max_len = max_len
+        """Build sinusoidal embeddings.
+        This matches the implementation in tensor2tensor, but differs slightly
+        from the description in Section 3.5 of "Attention Is All You Need".
+        """
+        half_dim = self.embedding_dim // 2 
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, dtype=torch.float) * -emb)
+        """
+        +2 is fairseq hack 
+        https://github.com/pytorch/fairseq/issues/1177
+        """
+        emb = torch.arange(self.max_len + 2, dtype=torch.float).unsqueeze(
+            1
+        ) * emb.unsqueeze(0)
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1).view(
+            self.max_len + 2, -1
         )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        if self.embedding_dim % 2 == 1:
+            # zero pad
+            emb = torch.cat([emb, torch.zeros(self.max_len + 2, 1)], dim=1)
+        """
+        precompute positions 
+        https://github.com/pytorch/fairseq/blob/b554f5ec9067c2f88bf17a736ddb57ea1cab14cc/fairseq/utils.py#L256
+        """
+        """
+        +2 is fairseq hack 
+        https://github.com/pytorch/fairseq/issues/1177
+        we adopt same design to facilitate using their saved model
+        """
+        position = torch.arange(0, self.max_len) + 2
+        pe = emb.index_select(0, position)
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
-
+    
     def forward(self, x): 
         # x size : B x T x d_model
         x = x + self.pe[:, :x.size(1)]
-        return self.dropout(x)
+        return x
+
+def main(): 
+
+    pe = PositionEmbedding(1024, 0.1)
+    print(pe.pe[0,:10,:4])
+    print()
+    print(pe.pe[0,:10,-5:])
+
+    # check fairseq's position embeddings
+    pefs = torch.load('../../tryfairseqwk103/reformat/pe_weights.pt')
+    d = pe.pe[0,:3072,:] - pefs[2:,:]
+    print(f"total difference^2 = {float((d**2).sum())}")
+
+if __name__ == '__main__': main()
